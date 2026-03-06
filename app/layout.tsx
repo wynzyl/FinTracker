de63c01f41daf3878984aa3a47f4573db2c7af08
wynzyl <wynzyltayag@gmail.com>
2026-03-06 10:52:06 +0800

    Update layout icons to use YambanLogo.jpg for all media types

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,15 +15,15 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: 'YambanLogo.jpg',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: 'YambanLogo.jpg',
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
+        url: 'YambanLogo.jpg',
         type: 'image/svg+xml',
       },
     ],
